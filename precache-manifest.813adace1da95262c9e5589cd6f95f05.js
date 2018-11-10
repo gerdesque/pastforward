636ef8c540b273901ef39b24b81134dd0ecfe535
Gerd Müller <gerdesk@posteo.org>
2018-11-10 21:44:25 +0100

    Updates

---
+++ precache-manifest.813adace1da95262c9e5589cd6f95f05.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4dfc9091d67b1e85e8ca23ad9a85dd41",
+    "url": "/pastforward/static/media/bg.4dfc9091.jpg"
+  },
+  {
+    "revision": "535afad2cb7ef0eb66d74f8907559867",
+    "url": "/pastforward/static/media/ModernGermanHandwriting.535afad2.ttf"
+  },
+  {
+    "revision": "d36b44b6a3db50effb9a46cb68e5627c",
+    "url": "/pastforward/static/media/GermaniaVersalien.d36b44b6.ttf"
+  },
+  {
+    "revision": "8faf7581001b26aa763f76f1ac6a7c58",
+    "url": "/pastforward/static/media/DistortionDosAnalogue.8faf7581.otf"
+  },
+  {
+    "revision": "d6b47d277073783c74d3",
+    "url": "/pastforward/static/js/runtime~main.d6b47d27.js"
+  },
+  {
+    "revision": "6269114b4715570ea71b",
+    "url": "/pastforward/static/js/main.6269114b.chunk.js"
+  },
+  {
+    "revision": "910e97fe3939efa2efb6",
+    "url": "/pastforward/static/js/1.910e97fe.chunk.js"
+  },
+  {
+    "revision": "6269114b4715570ea71b",
+    "url": "/pastforward/static/css/main.663e604c.chunk.css"
+  },
+  {
+    "revision": "08835941bd80f860c2493316c873cc39",
+    "url": "/pastforward/index.html"
+  }
+];
