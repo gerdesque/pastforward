7a2cec03e11b803cd560713a6acfd77d931dbad1
Gerd Müller <g.mueller@ewerk.com>
2018-11-20 14:16:47 +0100

    Remove header link in favor of outside click handler

--- src/App.js
+++ src/App.js
@@ -3,17 +3,12 @@ import './App.css';
 import ImageList from './ImageList';
 
 class App extends Component {
-  onClick = event => {
-    document.getElementsByTagName('video')[0].remove(document.getElementsByTagName('video')[0]);
-  };
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <a href="#top" className="App-Link" onClick={this.onClick}>
           <span className="App-past">past</span><span className="App-forward">forward</span>
-        </a>
         </header>
         <ImageList/>
       </div>
